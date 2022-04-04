018cbbe782368af87e02108017aa5303f7c6bc37
harley-zhang <harleyzgolego@gmail.com>
2022-04-03 21:32:01 -0400

    block to cover EE logo

---
+++ script.js
@@ -0,0 +1,13 @@
+/*var app = document.getElementById("app"); // assuming you object has id "obj"
+var doc = app.window.top.contentDocument.getElementById("header-menu");
+
+introJs().setOptions({
+  steps: [{
+    title: 'Welcome',
+    intro: 'Hello World! 👋'
+  },
+  {
+    element: el1,
+    intro: 'This step focuses on an image'
+  }]
+}).start();*/
