fec7be19ba5af34e75c12c3c149e5d28ac3b58c5
harley-zhang <harleyzgolego@gmail.com>
2024-07-09 18:02:36 -0400

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LaunchApp from "./pages/LaunchApp";
 
 
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/app" element={<LaunchApp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
