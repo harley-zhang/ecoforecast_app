84066e0965f9f8ee18a7615a2127da0717780567
harley-zhang <harleyzgolego@gmail.com>
2024-07-12 17:28:29 -0400

    fixed contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,71 +1,108 @@
+import React, { useState } from 'react';
 import Navbar from '../scenes/Navbar';
 import Footer from '../scenes/Footer';
 import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import useWeb3Forms from '@web3forms/react';
 
 const Contact = () => {
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
+  const { register, handleSubmit, reset } = useForm();
+  const { submit: onSubmitWeb3Forms } = useWeb3Forms({
+    access_key: '17eb3a6c-79d4-4872-8186-91716a32d0ec',
+    settings: {
+      from_name: 'EcoForecast',
+      subject: 'New Contact Message',
+    },
+    onSuccess: (msg, data) => {
+      reset(); // Reset form fields
+      setShowForm(false); // Hide the form on successful submission
+    },
+    onError: (msg, data) => {
+      console.error('Error submitting form:', msg);
+    },
+  });
 
-        formData.append("access_key", "Y17eb3a6c-79d4-4872-8186-91716a32d0ec");
+  const [showForm, setShowForm] = useState(true); // State to manage form visibility
 
-        const object = Object.fromEntries(formData);
-        const json = JSON.stringify(object);
+  const onSubmit = async (data) => {
+    try {
+      await onSubmitWeb3Forms(data);
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
 
-        const res = await fetch("https://api.web3forms.com/submit", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            },
-            body: json
-        }).then((res) => res.json());
+  const handleRetry = () => {
+    setShowForm(true); // Show the form again
+  };
 
-        if (res.success) {
-            console.log("Success", res);
-        }
-    };
+  return (
+    <>
+      <Helmet>
+        <title>Contact | EcoForecast</title>
+      </Helmet>
+      <Navbar />
 
-    return (
-        <>
-            <Helmet>
-                <title>Contact | EcoForecast</title>
-            </Helmet>
-            <Navbar />
-
-            <div className="sm:w-[750px] mx-auto px-5">
-                <h1 className="text-4xl xs:text-5xl sm:text-6xl mt-28 xs:mt-32 sm:mt-40 font-product-sans-regular mb-4 text-center">Contact our team</h1>
-                <div className="text-center">
-                    <p className="mt-6 xs:mt-8 font-product-sans-light-regular text-md xs:text-lg sm:text-xl text-center">We're happy to answer questions and get you acquainted with EcoForecast, including connecting you with helpful resources and exploring use cases for your team.
-                    </p>
-                </div>
-                <div className="text-sm font-product-sans-light-regular mt-24">
-                    <form onSubmit={onSubmit}>
-                        <h2 className="text-2xl mb-8 text-center">Contact</h2>
-                        <div className="mb-8">
-                            <label className="block mb-1" htmlFor="name">Name *</label>
-                            <input type="text" className="w-full border-grey rounded-md p-2 focus:border-black border focus:outline-none transition duration-300" id="name" name="name" required />
-                        </div>
-                        <div className="mb-8">
-                            <label className="block mb-1" htmlFor="email">Email *</label>
-                            <input type="email" className="w-full border-grey rounded-md p-2 focus:border-black border focus:outline-none transition duration-300" id="email" name="email" required />
-                        </div>
-                        <div className="mb-8">
-                            <label className="block mb-1" htmlFor="company">Company</label>
-                            <input type="text" className="w-full border-grey rounded-md p-2 focus:border-black border focus:outline-none transition duration-300" id="company" name="company" />
-                        </div>
-                        <div className="mb-8">
-                            <label className="block mb-1" htmlFor="message">Message *</label>
-                            <textarea className="w-full border-grey rounded-md p-2 h-52 resize-none focus:border-black border-2 focus:outline-none transition duration-300" id="message" name="message" required />
-                        </div>
-                        <button type="submit" className="bg-black rounded-full hover:bg-opacity-90 transition-all duration-300 text-white px-6 py-3">Submit</button>
-                    </form>
-                </div>
+      <div className="sm:w-[750px] mx-auto px-5">
+        <h1 className="text-4xl xs:text-5xl sm:text-6xl mt-28 xs:mt-32 sm:mt-40 font-product-sans-regular mb-4 text-center">
+          Contact our team
+        </h1>
+        <div className="text-center">
+          <p className="mt-6 xs:mt-8 font-product-sans-light-regular text-md xs:text-lg sm:text-xl text-center">
+            We're happy to answer questions and get you acquainted with EcoForecast, including connecting you with helpful resources and exploring use cases for your team.
+          </p>
+        </div>
+        <div className="text-sm font-product-sans-light-regular mt-24">
+          {showForm && (
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-8">
+                <label className="block mb-1">Name *</label>
+                <input
+                  type="text"
+                  className="w-full border-grey rounded-md p-2 focus:border-black border focus:outline-none transition duration-300"
+                  {...register('name', { required: true })}
+                />
+              </div>
+              <div className="mb-8">
+                <label className="block mb-1">Email *</label>
+                <input
+                  type="email"
+                  className="w-full border-grey rounded-md p-2 focus:border-black border focus:outline-none transition duration-300"
+                  {...register('email', { required: true })}
+                />
+              </div>
+              <div className="mb-8">
+                <label className="block mb-1">Message *</label>
+                <textarea
+                  className="w-full border-grey rounded-md p-2 h-52 resize-none focus:border-black border focus:outline-none transition duration-300"
+                  {...register('message', { required: true })}
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-black rounded-full hover:bg-opacity-90 transition-all duration-300 text-white px-6 py-3"
+              >
+                Submit
+              </button>
+            </form>
+          )}
+          {!showForm && (
+            <div className="w-[350px] xs:w-[400px] sm:w-[500px] mx-auto text-center mt-8">
+              <p className="text-lg">Thank you. Our team is looking forward to connecting with you. You'll hear from us soon!</p>
+              <button
+                onClick={handleRetry}
+                className="mt-4 bg-black rounded-full hover:bg-opacity-90 transition-all duration-300 text-white px-6 py-3"
+              >
+                Submit again
+              </button>
             </div>
+          )}
+        </div>
+      </div>
 
-            <Footer />
-        </>
-    )
-}
+      <Footer />
+    </>
+  );
+};
 
 export default Contact;
